e3b6651c0a1fb650c841a97be61ffbad4a53382a
Andreas Linnert <andreas@linnertmedia.de>
2021-08-03 01:31:26 +0200

    Fix position of centered PageContent scrollbars

--- components/ui/PageContent.tsx
+++ components/ui/PageContent.tsx
@@ -18,29 +18,30 @@ export const PageContent: FC<PropsWithChildren<Props>> = ({
   centered = false,
 }) => {
   return (
-    <div
-      className={classNames(
-        matchBoolToString(
-          centered,
-          'w-[600px] max-w-full mx-auto py-8',
-          'w-full py-4',
-        ),
-        'px-4 h-full overflow-y-auto',
-      )}
-    >
-      {title !== undefined ? (
-        <div className="text-2xl font-semibold mb-4 flex items-center">
-          {titleIcon !== undefined ? (
-            <div className={classNames('flex-0 mr-2')}>
-              <Heroicon icon={titleIcon} color={titleIconColor} scale={1.5} />
-            </div>
-          ) : null}
+    <div className="px-4 h-full overflow-y-auto">
+      <div
+        className={classNames(
+          matchBoolToString(
+            centered,
+            'w-[600px] max-w-full mx-auto py-8',
+            'w-full py-4',
+          ),
+        )}
+      >
+        {title !== undefined ? (
+          <div className="text-2xl font-semibold mb-4 flex items-center">
+            {titleIcon !== undefined ? (
+              <div className={classNames('flex-0 mr-2')}>
+                <Heroicon icon={titleIcon} color={titleIconColor} scale={1.5} />
+              </div>
+            ) : null}
 
-          <div className="flex-1 select-text">{title}</div>
-        </div>
-      ) : null}
+            <div className="flex-1 select-text">{title}</div>
+          </div>
+        ) : null}
 
-      {children}
+        {children}
+      </div>
     </div>
   )
 }
