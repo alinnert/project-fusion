f53ea88b9335090f2aa21191d77f8ef65968b050
Andreas Linnert <andreas@linnertmedia.de>
2021-08-06 13:16:39 +0200

    Format code

--- components/usePersistedFileHandle.tsx
+++ components/usePersistedFileHandle.tsx
@@ -1,24 +1,23 @@
-import { get } from 'idb-keyval';
-import { useEffect } from 'react';
-import { useAppDispatch } from '../redux';
-import { openDatabaseFielWithHandle } from '../redux/database/openDatabaseFileWithHandle';
-import { asyncTry } from '../utils/tryCatch';
+import { get } from 'idb-keyval'
+import { useEffect } from 'react'
+import { useAppDispatch } from '../redux'
+import { openDatabaseFielWithHandle } from '../redux/database/openDatabaseFileWithHandle'
+import { asyncTry } from '../utils/tryCatch'
 
 export function usePersistedFileHandle() {
-  const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch()
 
   useEffect(() => {
     async function run() {
-      const fileHandleResult = await asyncTry(() => get<FileSystemFileHandle>('fileHandle')
-      );
-      if (fileHandleResult.caught)
-        return;
-      if (fileHandleResult.value === undefined)
-        return;
-      const fileHandle = fileHandleResult.value;
-      openDatabaseFielWithHandle(fileHandle);
+      const fileHandleResult = await asyncTry(() =>
+        get<FileSystemFileHandle>('fileHandle'),
+      )
+      if (fileHandleResult.caught) return
+      if (fileHandleResult.value === undefined) return
+      const fileHandle = fileHandleResult.value
+      openDatabaseFielWithHandle(fileHandle)
     }
 
-    run();
-  }, [dispatch]);
+    run()
+  }, [dispatch])
 }
